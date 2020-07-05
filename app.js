646b5eaf1186de790a6f4fc03255e65bfcb45ca8
parkerdi12 <shafickovd@yandex.ru>
2020-07-05 14:54:48 +0400

    second commit

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
+// Modal
 
-var modal = document.getElementById('myModal');
+var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 
-
 btn.onclick = function () {
-    modal.style.display = "block";
-}
+  modal.style.display = "block";
+};
 
 window.onclick = function (event) {
-    if(event.target == modal) {
-        modal.style.display = "none";
-    }
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+// Авторизация
+
+document.getElementById("check").onclick = function () {
+  let login = document.getElementById("login").value;
+  let result = login;
+  document.getElementById('result-login').innerHTML = result;
 };
 
 
